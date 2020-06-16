7f10e2c6f7a8957ffc7fd560a81386dd9341b85a
Shureek Yunusov <mopkobka97@yandex.ru>
2020-06-16 12:50:16 +0300

    body fixed

--- backend/src/main/java/com/itmo/mse/soft/entity/Body.java
+++ backend/src/main/java/com/itmo/mse/soft/entity/Body.java
@@ -1,4 +1,15 @@
 package com.itmo.mse.soft.entity;
 
+import java.math.BigInteger;
+import javax.persistence.*;
+
+@Entity
 public class Body {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    protected BigInteger id;
+    protected BigInteger paymentId;
+    protected String barCode;
+    protected String state;
 }
