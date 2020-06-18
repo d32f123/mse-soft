bb8a4e5e69bfbfea8d0e7aba1b8786ed0f446860
Andrey Nesterov <andrey.nesterov@netcracker.com>
2020-06-18 19:15:08 +0300

    OrderApi Stub

--- backend/src/main/java/com/itmo/mse/soft/api/hydra/OrderAPI.java
+++ backend/src/main/java/com/itmo/mse/soft/api/hydra/OrderAPI.java
@@ -1,4 +1,38 @@
 package com.itmo.mse.soft.api.hydra;
 
+import com.itmo.mse.soft.order.Order;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Queue;
+
+@Service
+@Slf4j
 public class OrderAPI {
+    Queue<Order> orderQueue = new LinkedList<>();
+
+    public OrderAPI() {
+        this.orderQueue.add(Order.builder()
+            .paymentAmount(new BigDecimal("5556.00"))
+            .paymentInstant(Instant.now()).build());
+        this.orderQueue.add(Order.builder()
+        .paymentInstant(Instant.now().plus(Duration.ofMinutes(5)))
+        .paymentAmount(new BigDecimal("3333.00")).build());
+    }
+
+    public List<Order> receiveNewOrders() {
+        return List.of(Objects.requireNonNull(orderQueue.poll()));
+    }
+
+    public void confirmOrder(Order order, String bitcoinAddress, String bodyStateUrl) {
+    }
+
+    public void setTimeSlotInfo(List<TimeSlotInfo> timeSlotInfos) {
+    }
 }
--- backend/src/main/java/com/itmo/mse/soft/task/TaskManager.java
+++ backend/src/main/java/com/itmo/mse/soft/task/TaskManager.java
@@ -1,4 +1,22 @@
 package com.itmo.mse.soft.task;
 
+import com.itmo.mse.soft.api.hydra.OrderAPI;
+import com.itmo.mse.soft.schedule.ScheduleManager;
+import com.itmo.mse.soft.service.BodyService;
+import com.itmo.mse.soft.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskManager {
+
+    @Autowired
+    BodyService bodyService;
+    @Autowired
+    EmployeeService employeeService;
+    @Autowired
+    ScheduleManager scheduleManager;
+    @Autowired
+    OrderAPI orderAPI;
+
 }
