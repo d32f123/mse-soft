dea61c39fb24fdc4121f96502e2186214eb14f24
Oleg Romanov <romanov-97@mail.ru>
2020-06-19 04:07:36 +0300

    Add redirect to task page

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -2,6 +2,8 @@ let host = "http://localhost:8080";
 let authUrl = host + "/api/v1/auth";
 let dailyTasksUrl = host + "/api/v1/employees/daily-tasks";
 
+let taskUrl = "task.html";
+
 function auth(data, success, error){
     $.ajax({
         type: "GET",
@@ -30,4 +32,38 @@ function redirectToGroomerPage(token) {
     window.open("groomer.html?token="+token,"_self");
 
     console.log("setUpGroomerPage finish");
+}
+
+function createGroomerTable(token) {
+    console.log("createGroomerTable(" + token + ") start");
+
+    getTasks(token, function (tasks) {
+        console.log(tasks);
+
+        let tbody = document.getElementById("tbody");
+
+        for (let task of tasks) {
+            let tr = document.createElement("tr");
+
+            let type = document.createElement("th");
+            let a = document.createElement("a");
+            a.innerText = task["taskType"];
+            a.href = taskUrl + "?token=" + token + "&task=" + task['taskId'];
+            type.appendChild(a);
+
+            let timeStart = document.createElement("th");
+            timeStart.innerText = task["scheduleEntry"]["timeStart"];
+
+            let timeEnd = document.createElement("th");
+            timeEnd.innerText = task["scheduleEntry"]["timeEnd"];
+
+            tr.appendChild(type);
+            tr.appendChild(timeStart);
+            tr.appendChild(timeEnd);
+
+            tbody.appendChild(tr);
+        }
+    });
+
+    console.log("createGroomerTable() finish");
 }
