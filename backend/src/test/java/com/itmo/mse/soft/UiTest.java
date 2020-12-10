31ad53271c91d84c5c47251cb9f3e006e0e156c7
Oleg Romanov <romanov-97@mail.ru>
2020-12-10 16:48:16 +0300

    Add some tests

--- backend/src/test/java/com/itmo/mse/soft/UiTest.java
+++ backend/src/test/java/com/itmo/mse/soft/UiTest.java
@@ -1,13 +1,11 @@
 package com.itmo.mse.soft;
 
+import okhttp3.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,6 +17,7 @@ public class UiTest {
     private final String groomer_password = ConfProperties.getProperty("groomer_password");
     private final String pig_master_login = ConfProperties.getProperty("pig_master_login");
     private final String pig_master_password = ConfProperties.getProperty("pig_master_password");
+    private final String hydra_orders_url = ConfProperties.getProperty("hydra_orders_url");
 
     WebDriver get_driver(){
         System.setProperty("webdriver.firefox.driver", firefoxDriver);
@@ -26,6 +25,20 @@ public class UiTest {
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
+    }
+
+    void sendPostRequest(String url, String content){
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, content);
+        Request request = new Request.Builder()
+                .url(url)
+                .method("POST", body)
+                .addHeader("Content-Type", "application/json")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+        } catch (java.io.IOException e) {}
     }
 
     void login(WebDriver driver, String login, String password){
@@ -103,4 +116,51 @@ public class UiTest {
 
         driver.close();
     }
+
+    @Test
+    void auth1() {
+        WebDriver driver = get_driver();
+
+        login(driver, groomer_login, "123456");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String titleText = driver.findElement(By.tagName("h1")).getText();
+        assertThat(titleText).isNotEqualTo("Домашняя страничка Грумера");
+
+        driver.close();
+    }
+
+    @Test
+    void auth2() {
+        WebDriver driver = get_driver();
+
+        login(driver, groomer_login, "qwerty");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String titleText = driver.findElement(By.tagName("h1")).getText();
+        assertThat(titleText).isNotEqualTo("Домашняя страничка Грумера");
+
+        driver.close();
+    }
+
+    @Test
+    void auth3() {
+        WebDriver driver = get_driver();
+
+        login(driver, "sdasd", "asdasd");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String titleText = driver.findElement(By.tagName("h1")).getText();
+        assertThat(titleText).isNotEqualTo("Домашняя страничка Грумера");
+
+        driver.close();
+    }
+
+//    @Test
+//    void Feed(){
+//        sendPostRequest(
+//                hydra_orders_url,
+//                "{\"paymentAmount\": 100,\"pickupInstant\": \"1990-10-10T07:10:10.000+00:00\"}"
+//        );
+//    }
 }
