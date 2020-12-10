5eb985c7bc7f1dddd95e124e1562e88f43131967
Aleksandr Iunusov <aleksandr_iunusov@epam.com>
2020-12-11 02:00:49 +0300

    update task showing order and appearance

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -7,315 +7,333 @@ let completeSubTaskUrl = host + "/api/v1/employees/complete-sub-task";
 
 let taskUrl = "task.html";
 
-function auth(data, success, error){
-    $.ajax({
-        type: "POST",
-        url: authUrl,
-        data: JSON.stringify(data),
-        success: success,
-        error: error,
-        dataType: "text",
-        headers: {"Content-Type": "application/json"}
-    });
+function auth(data, success, error) {
+  $.ajax({
+    type: "POST",
+    url: authUrl,
+    data: JSON.stringify(data),
+    success: success,
+    error: error,
+    dataType: "text",
+    headers: {"Content-Type": "application/json"}
+  });
 }
 
 function getTokenFromUrl() {
-    let token = window.location.search.substr(1).split("=")[1];
-    token = token.split("&")[0];
+  let token = window.location.search.substr(1).split("=")[1];
+  token = token.split("&")[0];
 
-    console.log("token: " + token);
-    return token;
+  console.log("token: " + token);
+  return token;
 }
 
 function logout() {
-    console.log("redirectToIndexPage() started");
-    window.open("index.html","_self");
-    console.log("redirectToindexPage() finished");
+  console.log("redirectToIndexPage() started");
+  window.open("index.html", "_self");
+  console.log("redirectToindexPage() finished");
 }
 
 function getTasks(token, success, error) {
-    $.ajax({
-        type: "GET",
-        url: dailyTasksUrl,
-        success: success,
-        error: error,
-        dataType: "json",
-        headers: {Token: token}
-    });
+  $.ajax({
+    type: "GET",
+    url: dailyTasksUrl,
+    success: success,
+    error: error,
+    dataType: "json",
+    headers: {Token: token}
+  });
 }
 
 function redirectToGroomerPage(token) {
-    console.log("redirectToGroomerPage(" + token + ") start");
+  console.log("redirectToGroomerPage(" + token + ") start");
 
-    window.open("groomer.html?token="+token,"_self");
+  window.open("groomer.html?token=" + token, "_self");
 
-    console.log("redirectToGroomerPage finish");
+  console.log("redirectToGroomerPage finish");
 }
 
 function redirectToPigMasterPage(token) {
-    console.log("redirectToPigMasterPage(" + token + ") start");
+  console.log("redirectToPigMasterPage(" + token + ") start");
 
-    window.open("pig_master.html?token=" + token, "_self");
+  window.open("pig_master.html?token=" + token, "_self");
 
-    console.log("redirectToPigMasterPage finish");
+  console.log("redirectToPigMasterPage finish");
 }
 
 function createGroomerTable(token) {
-    console.log("createGroomerTable(" + token + ") start");
+  console.log("createGroomerTable(" + token + ") start");
 
-    getTasks(token, function (tasks) {
-        console.log(tasks);
+  getTasks(token, function (tasks) {
+    console.log(tasks);
 
-        let tbody = document.getElementById("tbody");
+    let tbody = document.getElementById("tbody");
 
-        for (let task of tasks) {
-            let tr = document.createElement("tr");
-            if (task.complete) {
-                tr.classList.add("_complete");
-            }
+    for (let task of tasks.filter(task =>
+        task.body.state !== "AWAITING_RECEIVAL"
+        || task.body.state === "AWAITING_RECEIVAL"
+        && task.taskType === "PICKUP"
+    )) {
+      let tr = document.createElement("tr");
+      if (task.complete) {
+        tr.classList.add("_complete");
+      }
 
-            let type = document.createElement("th");
-            let a = document.createElement("a");
-            a.innerText = task["taskType"];
-            a.href = taskUrl + "?token=" + token + "&task=" + task['taskId'];
-            a.setAttribute("name", task["taskType"]);
-            type.appendChild(a);
+      let type = document.createElement("th");
+      let a = document.createElement("a");
+      a.innerText = task["taskType"];
+      a.href = taskUrl + "?token=" + token + "&task=" + task['taskId'];
+      a.setAttribute("name", task["taskType"]);
+      type.appendChild(a);
 
-            let timeStart = document.createElement("th");
-            timeStart.innerText = getFormattedTime(task["scheduleEntry"]["timeStart"]);
+      let timeStart = document.createElement("th");
+      timeStart.innerText = getFormattedTime(
+          task["scheduleEntry"]["timeStart"]);
 
-            let timeEnd = document.createElement("th");
-            timeEnd.innerText = getFormattedTime(task["scheduleEntry"]["timeEnd"]);
+      let timeEnd = document.createElement("th");
+      timeEnd.innerText = getFormattedTime(task["scheduleEntry"]["timeEnd"]);
 
-            tr.appendChild(type);
-            tr.appendChild(timeStart);
-            tr.appendChild(timeEnd);
+      tr.appendChild(type);
+      tr.appendChild(timeStart);
+      tr.appendChild(timeEnd);
 
-            tbody.appendChild(tr);
-        }
-    });
+      tbody.appendChild(tr);
+    }
+  });
 
-    console.log("createGroomerTable() finish");
+  console.log("createGroomerTable() finish");
 }
 
 function createPigMasterTable(token) {
-    console.log("createPigMasterTable(" + token + ") start");
+  console.log("createPigMasterTable(" + token + ") start");
 
-    getTasks(token, function (tasks) {
-        console.log(tasks);
+  getTasks(token, function (tasks) {
+    console.log(tasks);
 
-        let tbody = document.getElementById("tbody");
-        let possible = ["IN_FEEDING", "FED", "GROOMED"];
-        for (let task of tasks.filter(task => possible.includes(task.body.state))) {
-            let tr = document.createElement("tr");
-            if (task.complete) {
-                tr.classList.add("_complete");
-            }
+    let tbody = document.getElementById("tbody");
+    let possible = ["IN_FEEDING", "FED", "GROOMED"];
+    for (let task of tasks.filter(task => possible.includes(task.body.state))) {
+      let tr = document.createElement("tr");
+      if (task.complete) {
+        tr.classList.add("_complete");
+      }
 
-            let type = document.createElement("th");
-            let a = document.createElement("a");
-            a.innerText = task["taskType"];
-            a.href = taskUrl + "?token=" + token + "&task=" + task['taskId'];
-            type.appendChild(a);
+      let type = document.createElement("th");
+      let a = document.createElement("a");
+      a.innerText = task["taskType"];
+      a.href = taskUrl + "?token=" + token + "&task=" + task['taskId'];
+      type.appendChild(a);
 
-            let timeStart = document.createElement("th");
-            timeStart.innerText = getFormattedTime(task["scheduleEntry"]["timeStart"]);
+      let timeStart = document.createElement("th");
+      timeStart.innerText = getFormattedTime(
+          task["scheduleEntry"]["timeStart"]);
 
-            let timeEnd = document.createElement("th");
-            timeEnd.innerText = getFormattedTime(task["scheduleEntry"]["timeEnd"]);
+      let timeEnd = document.createElement("th");
+      timeEnd.innerText = getFormattedTime(task["scheduleEntry"]["timeEnd"]);
 
-            let pigstyNumber = document.createElement("th");
-            let pigAmount = document.createElement("th");
-            let lastFedTask = document.createElement("th");
+      let pigstyNumber = document.createElement("th");
+      let pigAmount = document.createElement("th");
+      let lastFedTask = document.createElement("th");
 
-            if (task['pigsty']) {
-                pigstyNumber.innerText = task['pigsty']['pigstyNumber'];
-                pigAmount.innerText = task['pigsty']['pigAmount'];
-                lastFedTask.innerText = getFormattedDateTime(task['pigsty']['lastFeedTime']);
-            }
+      if (task['pigsty']) {
+        pigstyNumber.innerText = task['pigsty']['pigstyNumber'];
+        pigAmount.innerText = task['pigsty']['pigAmount'];
+        lastFedTask.innerText = getFormattedDateTime(
+            task['pigsty']['lastFeedTime']);
+      }
 
-            tr.appendChild(type);
-            tr.appendChild(timeStart);
-            tr.appendChild(timeEnd);
-            tr.appendChild(pigstyNumber);
-            tr.appendChild(pigAmount);
-            tr.appendChild(lastFedTask);
+      tr.appendChild(type);
+      tr.appendChild(timeStart);
+      tr.appendChild(timeEnd);
+      tr.appendChild(pigstyNumber);
+      tr.appendChild(pigAmount);
+      tr.appendChild(lastFedTask);
 
-            tbody.appendChild(tr);
-        }
-    });
+      tbody.appendChild(tr);
+    }
+  });
 
-    console.log("createPigMasterTable() finish");
+  console.log("createPigMasterTable() finish");
 }
 
 // task page
 function getTaskPageParams() {
-    let token = window.location.search.substr(1).split("&")[0].split("=")[1];
-    let taskId = window.location.search.substr(1).split("&")[1].split("=")[1];
-    let dashboardUrl = document.referrer;
-    console.log("token:\t" + token);
-    console.log("taskId:\t" + taskId);
+  let token = window.location.search.substr(1).split("&")[0].split("=")[1];
+  let taskId = window.location.search.substr(1).split("&")[1].split("=")[1];
+  let dashboardUrl = document.referrer;
+  console.log("token:\t" + token);
+  console.log("taskId:\t" + taskId);
 
-    return {token, taskId, dashboardUrl};
+  return {token, taskId, dashboardUrl};
 }
 
 function getFormattedTime(timeString) {
-    return new Date(timeString).toLocaleTimeString('ru-RU');
+  return new Date(timeString).toLocaleTimeString('ru-RU');
 }
 
 function getFormattedDateTime(dateTimeString) {
-    return new Date(dateTimeString).toLocaleString('ru-RU');
+  return new Date(dateTimeString).toLocaleString('ru-RU');
 }
 
 function setUpTaskPage(token, taskId, dashboardUrl) {
-    console.log("loadTaskInfo(" + token + "," + taskId + ") start");
+  console.log("loadTaskInfo(" + token + "," + taskId + ") start");
 
-    getTasks(token, function (tasks) {
-        console.log(tasks);
+  getTasks(token, function (tasks) {
+    console.log(tasks);
 
-        let curTask = null;
+    let curTask = null;
 
-        for (let task of tasks) {
-            if (task["taskId"] === taskId) {
-                curTask = task;
-                break;
-            }
-        }
+    for (let task of tasks) {
+      if (task["taskId"] === taskId) {
+        curTask = task;
+        break;
+      }
+    }
 
-        // show task completing
-        document.querySelector("h1").innerText = curTask['taskType'];
-        if (curTask["complete"]) {
-            showTaskComplete();
-        }
+    // show task completing
+    document.querySelector("h1").innerText = curTask['taskType'];
+    if (curTask["complete"]) {
+      showTaskComplete();
+    }
 
-        // show task info
-        document.getElementById("status").innerText = curTask['body']["state"];
-        document.getElementById("barcode").innerText = curTask['body']["barcode"];
-        document.getElementById("planed_time").innerText = getFormattedTime(curTask["scheduleEntry"]["timeStart"]);
-        const dashboard = new URL(dashboardUrl);
-        document.getElementById("schedule").setAttribute("href", `${dashboard.pathname}?token=${token}`);
+    // show task info
+    document.getElementById("status").innerText = curTask['body']["state"];
+    document.getElementById("barcode").innerText = curTask['body']["barcode"];
+    document.getElementById("planed_time").innerText = getFormattedTime(
+        curTask["scheduleEntry"]["timeStart"]);
+    const dashboard = new URL(dashboardUrl);
+    document.getElementById("schedule").setAttribute("href",
+        `${dashboard.pathname}?token=${token}`);
 
-        let actions = document.getElementById("actions")
+    let actions = document.getElementById("actions")
 
-        for (let subTask of curTask["subTasks"]) {
-            let div = document.createElement("div");
-            div.setAttribute("class", "form-check mb-2");
+    let sorted = curTask.subTasks.sort((a, b) => Date.parse(a.scheduleEntry.timeStart) - Date.parse(b.scheduleEntry.timeStart))
+    let doStop = false;
 
-            let input = document.createElement("input");
-            input.setAttribute("class", "form-check-input");
-            input.setAttribute("type", "checkbox");
-            input.setAttribute("class", "form-check-input");
-            input.setAttribute("value", subTask["subTaskType"]);
-            input.setAttribute("id", subTask["subTaskId"]);
-            if (subTask['complete']) {
-                input.setAttribute("disabled", "disabled");
-            }
+    for (let subTask of sorted) {
+      let div = document.createElement("div");
+      div.setAttribute("class", "form-check mb-2");
 
-            let label = document.createElement("label");
-            label.setAttribute("class", "form-check-label");
-            label.setAttribute("for", subTask["subTaskType"]);
-            label.innerText = subTask["subTaskType"];
+      let input = document.createElement("input");
+      input.setAttribute("class", "form-check-input");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("class", "form-check-input");
+      input.setAttribute("value", subTask["subTaskType"]);
+      input.setAttribute("id", subTask["subTaskId"]);
+      if (subTask['complete']) {
+        input.setAttribute("disabled", "disabled");
+      }
 
-            div.appendChild(input);
-            div.appendChild(label);
-            actions.appendChild(div);
-        }
-    });
+      let label = document.createElement("label");
+      label.setAttribute("class", "form-check-label");
+      label.setAttribute("for", subTask["subTaskType"]);
+      label.innerText = subTask["subTaskType"];
 
-    console.log("loadTaskInfo() finish");
+      div.appendChild(input);
+      div.appendChild(label);
+      actions.appendChild(div);
+      if (!subTask.complete)
+        doStop = true;
+      if (doStop)
+          break;
+    }
+  });
+
+  console.log("loadTaskInfo() finish");
 }
 
 function completeSubTask(token, subTaskId) {
-    console.log("completeSubTask(" + token + ", " + subTaskId + ") start");
+  console.log("completeSubTask(" + token + ", " + subTaskId + ") start");
 
-    $.ajax({
-        type: "POST",
-        url: completeSubTaskUrl + "/" + subTaskId,
-        dataType: "json",
-        headers: {Token: token},
-        success: function (response) {
-            let input = document.getElementById(subTaskId);
-            input.setAttribute("disabled","disabled");
+  $.ajax({
+    type: "POST",
+    url: completeSubTaskUrl + "/" + subTaskId,
+    dataType: "json",
+    headers: {Token: token},
+    success: function (response) {
+      let input = document.getElementById(subTaskId);
+      input.setAttribute("disabled", "disabled");
 
-            console.log("completeSubTask(" + token + ", " + subTaskId + ") finish");
-        },
-    });
+      console.log("completeSubTask(" + token + ", " + subTaskId + ") finish");
+    },
+  });
 }
 
 function showTaskComplete() {
-    document.getElementById("bodyTitle").setAttribute(
-        "class", "alert alert-success"
-    );
+  document.getElementById("bodyTitle").setAttribute(
+      "class", "alert alert-success"
+  );
 
-    document.getElementById("btnCompleteTask").setAttribute("disabled", "disabled");
-    document.getElementById("btnCompleteSubTask").setAttribute("disabled", "disabled");
+  document.getElementById("btnCompleteTask").setAttribute("disabled",
+      "disabled");
+  document.getElementById("btnCompleteSubTask").setAttribute("disabled",
+      "disabled");
 }
 
 function completeSubTasks() {
-    console.log("completeSubTasks() start");
+  console.log("completeSubTasks() start");
 
-    let actions = document.getElementById("actions");
-    let params = getTaskPageParams();
-    let checkedSubTasks = null;
+  let actions = document.getElementById("actions");
+  let params = getTaskPageParams();
+  let checkedSubTasks = null;
 
-    // get checked actions
-    for (let actionDiv of actions.querySelectorAll("div")) {
-        let input = actionDiv.querySelector("input");
+  // get checked actions
+  for (let actionDiv of actions.querySelectorAll("div")) {
+    let input = actionDiv.querySelector("input");
 
-        if (input.checked) {
-            completeSubTask(params["token"], input.getAttribute("id"));
-        }
+    if (input.checked) {
+      completeSubTask(params["token"], input.getAttribute("id"));
     }
+  }
 
-    console.log("completeSubTasks() finish");
+  console.log("completeSubTasks() finish");
 }
 
 function completeTask() {
-    console.log("completeTask() start");
+  console.log("completeTask() start");
 
-    let params = getTaskPageParams();
-    let taskId = params['taskId'];
-    let token = params['token'];
+  let params = getTaskPageParams();
+  let taskId = params['taskId'];
+  let token = params['token'];
 
-    $.ajax({
-        type: "POST",
-        url: completeTaskUrl + "/" + taskId,
-        dataType: "json",
-        headers: {Token: token},
-        success: function (response) {
-            showTaskComplete();
-            console.log("completeTask(" + token + ", " + taskId + ") finish");
-        },
-    });
+  $.ajax({
+    type: "POST",
+    url: completeTaskUrl + "/" + taskId,
+    dataType: "json",
+    headers: {Token: token},
+    success: function (response) {
+      showTaskComplete();
+      console.log("completeTask(" + token + ", " + taskId + ") finish");
+    },
+  });
 
-    console.log("completeTask() finish");
+  console.log("completeTask() finish");
 }
 
 function getRoleSync(token, success, error) {
-    console.log("getRole() started");
-    $.ajax({
-        async: false,
-        type: "GET",
-        url: getRoleUrl,
-        success: success,
-        error: error,
-        dataType: "json",
-        headers: {Token: token}
-    });
-    console.log("getRole() finished");
+  console.log("getRole() started");
+  $.ajax({
+    async: false,
+    type: "GET",
+    url: getRoleUrl,
+    success: success,
+    error: error,
+    dataType: "json",
+    headers: {Token: token}
+  });
+  console.log("getRole() finished");
 }
 
 function getRole(token) {
-    console.log("getRole() start");
-    getRole.role = null;
+  console.log("getRole() start");
+  getRole.role = null;
 
-    getRoleSync(token, function (response) {
-        console.log("Response: " + response);
-        if (response.length > 0) {
-            getRole.role = response;
-        }
-    }, null);
+  getRoleSync(token, function (response) {
+    console.log("Response: " + response);
+    if (response.length > 0) {
+      getRole.role = response;
+    }
+  }, null);
 
-    console.log("getRole() -> " + getRole.role);
-    return getRole.role;
+  console.log("getRole() -> " + getRole.role);
+  return getRole.role;
 }
